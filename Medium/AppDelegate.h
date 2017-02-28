834816448522c8c17b04043b0e9571b6ecdb278a
Matt Guest <theguestfamily@thes-macbook-pro.local>
2017-02-27 21:16:38 -0800

    Initial Commit

---
+++ Medium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Medium
+//
+//  Created by The Guest Family on 2/27/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Medium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Medium
+//
+//  Created by The Guest Family on 2/27/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
